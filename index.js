a31e76d3856ec1f4bd7b399983e52817138c0a1a
rakesh-raikwar <rakesh.raikwar@thetechharvest.com>
2026-01-02 22:55:59 +0530

    cors origin

--- index.js
+++ index.js
@@ -16,26 +16,19 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
-const allowedOrigins = [
-  "https://rakesh-raikwar.vercel.app",
-  "http://localhost:5173",
-];
-
 app.use(
   cors({
-    origin: function (origin, callback){
-      if(!origin) return callback(null, true);
-      if(allowedOrigins.includes(origin)){
-        callback(null, true);
-      } else{
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: [
+      "https://rakesh-raikwar.vercel.app",
+      "http://localhost:5173",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
+
+app.options("*", cors());
 
 app.use(express.json());
 
@@ -106,8 +99,6 @@ app.get("/", (req, res) => {
 // app.get("/", (req, res) => {
 //   res.redirect(process.env.FRONTEND_URL);
 // });
-
-app.options("*", cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/admin", adminRoutes);
