f2a23b723dd10fc5897079fbda78efccdaa40f0f
rakesh-raikwar <rakesh.raikwar@thetechharvest.com>
2026-01-02 16:54:20 +0530

    cors

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ app.use(
   cors({
     origin: [FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    // methods: ["GET", "POST", "PUT", "DELETE"],
+    // allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
 
